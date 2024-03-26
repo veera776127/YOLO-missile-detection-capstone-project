75d55cf14c5ff6ec9efc883a735baef60992a00a
bkuruban@mail.yu.edu <125217469+KURUBANAGARAJ@users.noreply.github.com>
2024-03-26 00:17:00 -0400

    code update

--- app1.py
+++ app1.py
@@ -99,6 +99,11 @@ def uploaded_file(filename):
         return send_from_directory(app.config['RESULTS_FOLDER'], filename)
     else:
         return "File not found", 404
+    
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
+
+
